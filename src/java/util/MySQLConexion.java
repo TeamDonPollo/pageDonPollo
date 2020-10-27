69e988d2186cfe431ffe0f93a4451a28872df6fd
GIOVANA <GIOVANA@JAC>
2020-10-27 02:15:34 -0500

    actualización de paquete util

--- src/java/util/MySQLConexion.java
+++ src/java/util/MySQLConexion.java
@@ -4,9 +4,9 @@ public class MySQLConexion {
 	public static Connection getConexion(){ 
 		Connection con = null;
 		try { Class.forName("com.mysql.jdbc.Driver"); 
-		String url = "jdbc:mysql://localhost/polleria"; 
-		String usr = "root";
-		String psw = ""; 
+		String url = "jdbc:mysql://mysql5030.site4now.net/db_a6978f_prueba"; 
+		String usr = "a6978f_prueba";
+		String psw = "prueba123"; 
 		con = DriverManager.getConnection(url,usr,psw); 
 		System.out.println("conexion ok");
 		} catch (ClassNotFoundException ex)
